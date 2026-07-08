5c7ba1b0317e6c6fde7f511120ca3ba4e5bacd6b
Lev Kadatskyi <levkadatscky@gmail.com>
2026-07-08 14:43:23 +0300

    chore: created frontend project

---
+++ frontend/src/main.ts
@@ -0,0 +1,10 @@
+import { VueQueryPlugin } from '@tanstack/vue-query';
+import { createPinia } from 'pinia';
+import { createApp } from 'vue';
+import App from './App.vue';
+import './style.css';
+
+const app = createApp(App);
+app.use(createPinia());
+app.use(VueQueryPlugin);
+app.mount('#app');
