6f3e662c13358a469c2632b39394e2808dfc115b
Lev Kadatskyi <levkadatscky@gmail.com>
2026-07-15 17:13:55 +0300

    feat: create DashboardPage component to display user email on login

---
+++ frontend/src/pages/DashboardPage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/auth';
+
+const auth = useAuthStore();
+</script>
+
+<template>
+  <div>
+    <h1 class="text-xl font-bold">Dashboard</h1>
+    <p>Logged in as {{ auth.user?.email }}</p>
+  </div>
+</template>
