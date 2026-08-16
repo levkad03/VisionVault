07c8590775e2cbe90a44a5a24cd28f362ce69762
Lev Kadatskyi <levkadatscky@gmail.com>
2026-08-16 14:30:28 +0300

    feat: add route for image detail page

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -23,6 +23,11 @@ const router = createRouter({
         { path: '', name: 'dashboard', component: () => import('@/pages/DashboardPage.vue') },
         { path: 'upload', name: 'upload', component: () => import('@/pages/UploadPage.vue') },
         { path: 'gallery', name: 'gallery', component: () => import('@/pages/GalleryPage.vue') },
+        {
+          path: 'images/:id',
+          name: 'image-detail',
+          component: () => import('@/pages/ImageViewerPage.vue'),
+        },
         { path: 'search', name: 'search', component: () => import('@/pages/SearchPage.vue') },
       ],
     },
